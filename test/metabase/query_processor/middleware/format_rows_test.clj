77ee573cffc54cd95d6c20c6b7101788e8c5b416
Ryan Senior <ryan@metabase.com>
2017-10-05 14:16:57 -0500

    Disable Time column tests for mongo and redshift
    They don't have a Time datatype, so no need to test on those databases.

--- test/metabase/query_processor/middleware/format_rows_test.clj
+++ test/metabase/query_processor/middleware/format_rows_test.clj
@@ -10,7 +10,7 @@
             [metabase.query-processor-test :as qpt]
             [metabase.test.data.datasets :refer [*engine*]]))
 
-(qpt/expect-with-non-timeseries-dbs-except #{:oracle}
+(qpt/expect-with-non-timeseries-dbs-except #{:oracle :mongo :redshift}
   (if (= :sqlite *engine*)
     [[1 "Plato Yeshua" "2014-04-01 00:00:00" "08:30:00"]
      [2 "Felipinho Asklepios" "2014-12-05 00:00:00" "15:15:00"]
@@ -25,10 +25,11 @@
      [5 "Quentin Sören" "2014-10-03T00:00:00.000Z" "17:30:00.000Z"]])
   (->> (data/with-db (data/get-or-create-database! defs/test-data-with-time)
          (data/run-query users
+           (ql/order-by (ql/asc $id))
            (ql/limit 5)))
        qpt/rows))
 
-(qpt/expect-with-non-timeseries-dbs-except #{:oracle}
+(qpt/expect-with-non-timeseries-dbs-except #{:oracle :mongo :redshift}
   (cond
     (= :sqlite *engine*)
     [[1 "Plato Yeshua" "2014-04-01 00:00:00" "08:30:00"]
@@ -38,11 +39,11 @@
      [5 "Quentin Sören" "2014-10-03 00:00:00" "17:30:00"]]
 
     (qpt/supports-report-timezone? *engine*)
-    [[1 "Plato Yeshua" "2014-04-01T00:00:00.000Z" "00:30:00.000-08:00"]
-     [2 "Felipinho Asklepios" "2014-12-05T00:00:00.000Z" "07:15:00.000-08:00"]
-     [3 "Kaneonuskatew Eiran" "2014-11-06T00:00:00.000Z" "08:15:00.000-08:00"]
-     [4 "Simcha Yan" "2014-01-01T00:00:00.000Z" "00:30:00.000-08:00"]
-     [5 "Quentin Sören" "2014-10-03T00:00:00.000Z" "09:30:00.000-08:00"]]
+    [[1 "Plato Yeshua" "2014-04-01T00:00:00.000-07:00" "00:30:00.000-08:00"]
+     [2 "Felipinho Asklepios" "2014-12-05T00:00:00.000-08:00" "07:15:00.000-08:00"]
+     [3 "Kaneonuskatew Eiran" "2014-11-06T00:00:00.000-08:00" "08:15:00.000-08:00"]
+     [4 "Simcha Yan" "2014-01-01T00:00:00.000-08:00" "00:30:00.000-08:00"]
+     [5 "Quentin Sören" "2014-10-03T00:00:00.000-07:00" "09:30:00.000-08:00"]]
 
     :else
     [[1 "Plato Yeshua" "2014-04-01T00:00:00.000Z" "08:30:00.000Z"]
@@ -53,5 +54,6 @@
   (tu/with-temporary-setting-values [report-timezone "America/Los_Angeles"]
     (->> (data/with-db (data/get-or-create-database! defs/test-data-with-time)
            (data/run-query users
+             (ql/order-by (ql/asc $id))
              (ql/limit 5)))
          qpt/rows)))
